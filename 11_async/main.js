2590cbe5ca8f345df6214d75ab72bf117144ad54
tornikeo <tornikeonoprishvili@gmail.com>
2022-05-09 00:19:40 +0400

    Async chapter

--- 11_async/main.js
+++ 11_async/main.js
@@ -35,3 +35,38 @@
 // console.log('ABCD');
 // undefined.something;
 // setTimeout(() => console.log("Delayed stuff"), 200)
+
+class Timeout extends Error {}
+
+function request(nest, target, type, content) {
+    return new Promise((resolve, reject) => {
+        let done = false;
+        function attempt(n) {
+            nest.send(target, type, content, (failed,value) => {
+                done = true;
+                if (failed) reject(failed);
+                else resolve(value)
+            })
+            setTimeout(() => {
+                if (done) return;
+                else if (n < 3) attempt(n + 1)
+                else reject(new Timeout("Timed out"))
+            }, 250);
+        }
+        attempt(1)
+    })
+}
+
+
+function requestType(name, handler) {
+    defineRequestType(name, (nest, content, source, callback) => {
+        try {
+            Promise.resolve(handler(nest, content, source))
+                .then(response => callback)
+        } catch (error) {
+            
+        }
+    })
+}
+
+requestType
