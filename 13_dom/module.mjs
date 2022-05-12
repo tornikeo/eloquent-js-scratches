e8acbbb18e41fd979b0703d1acb6bd2f611d840d
tornikeo <tornikeonoprishvili@gmail.com>
2022-05-12 23:46:54 +0400

    More DOM chapter

--- 13_dom/module.mjs
+++ 13_dom/module.mjs
@@ -3,3 +3,29 @@
 export function func() {
     console.log('You import module.mjs');
 }
+// console.log( Node.ELEMENT_NODE );
+// // console.log(document.documentElement);  
+// console.log(document.body.nodeType);
+
+// console.log(document.body.nodeType === Node.ELEMENT_NODE)
+
+// console.log(document.body.childNodes);
+
+// console.log(document.body.childNodes[0].nextSibling.parentNode);
+
+function talksAbout(node, word) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        for (const child of node.childNodes) {
+            if (talksAbout(child, word)) {
+                return true;
+            }
+        }
+        return false;
+    } else if (node.nodeType == Node.TEXT_NODE) {
+        return node.nodeValue.indexOf(word) > -1;
+    }
+}
+
+// console.log(talksAbout(document.body, "book"));
+// console.log(talksAbout(document.body, "dragons"));
+
