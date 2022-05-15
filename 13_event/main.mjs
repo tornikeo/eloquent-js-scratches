1d9cc4d8dfbec143cc1054354ffa49f648cead50
tornikeo <tornikeonoprishvili@gmail.com>
2022-05-15 23:44:19 +0400

    Chapter 13, events

---
+++ 13_event/main.mjs
@@ -0,0 +1,15 @@
+// WWW is different from internet in that
+// WWW is a set of protocols that allow vising webpages
+// while internet is WWW + non-website type connections
+// like TCP and SMTP. WWW is a subset of Internet.
+
+// In WWW, machines have IPs which is a number format 
+// You can register instead a doman, which provides a
+// more convenient string name, which then points to the
+// said IP address.
+
+// HTML is a document format. It is interpreted by the 
+// browser to draw things on the screen.
+
+import { func } from "./module.mjs";
+func()
